dcc188558a63f86585e54dbd83889b373d78d2ec
tirtapir <tirtarumy14@gmail.com>
2024-09-23 21:52:37 +0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-Kidney-Diseases-Classification"
+AUTHOR_USER_NAME = "tirtapir"
+SRC_REPO = "CNNClassifier"
+AUTHOR_EMAIL = "tirtarumy14@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for kidney disease classification using CNN",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
