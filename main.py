44ab32c1185d4017ecb5f637591dce4f7ec2d970
tirtapir <tirtarumy14@gmail.com>
2024-09-24 09:49:47 +0700

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from src.CNNClassifier import logger
+from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
 
-logger.info("Test custom log")
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/CNNClassifier/utils/common.py
+++ src/CNNClassifier/utils/common.py
@@ -12,6 +12,32 @@ import base64
 
 
 @ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+
+@ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
